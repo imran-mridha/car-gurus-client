8e0c1dfbbb6747d9482328d65818eb6719a2b513
imran-mridha <mdimranmridha.pro@gmail.com>
2022-11-25 18:08:59 +0600

    Added products

--- src/Pages/CategoryProducts/CategoryProducts.jsx
+++ src/Pages/CategoryProducts/CategoryProducts.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import Product from './Product';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import Product from "./Product";
 
 const CategoryProducts = () => {
-  const products = useLoaderData()
+  const products = useLoaderData();
   console.log(products);
   return (
-    <div>
-      
-      {
-        products.map(product =>  <Product />)
-      }
+    <div className="container mx-auto my-20">
+      <div className="mx-5 md:mx-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+        {products.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
